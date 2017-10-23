9997f390a168075056b9f0cc4a2a642e00b8d8ad
budparr <budparr@gmail.com>
2017-10-23 13:37:19 -0400

    move back to a single directory

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import style from './../css/app.css';
+// import style from './../css/fonts-hoefler.css';
+
+import './lazysizes.js'
+import './listjs.js';
+import './menu-toggle.js';
+import './smoothscroll.js'
+import './nojs.js'
